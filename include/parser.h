40a8a006f3324b4c23bc86470aa38b6a239fb802
Y4ZK4Y <yk@yasamankarimi@gmail.com>
2024-08-06 11:57:51 +0200

    h

--- include/parser.h
+++ include/parser.h
@@ -4,7 +4,32 @@
 #include "lexer.h"
 
 
+/************************************************************/
+/*  Structs */
+
+/* nodes on the tree */
+// typedef struct s_node
+// {
+// 	what rule to followcommand/pipe
+// 	**args // does command have args
+// 	*exec path
+// 	fds[2]
+// 	s_list *redirects
+// 	struct s_node *right_node;
+// 	struct s_node *left_node;
+// }			t_node;
+
+
+
+
+
+/************************************************************/
 
 void    parser(t_tokenlist *tokenlist);
+// t_node	add_node();
+// t_node	create_tree(t_tokenlist *tokens);
+// int	syntax_check();
 
 #endif
+
+
