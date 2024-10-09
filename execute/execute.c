38d08470da7eea778f8000196d5750b6b0f8b2a3
Kimia Ziari <kziari@f1r6s9.codam.nl>
2024-10-09 16:19:22 +0200

    fix start of execution

--- execute/execute.c
+++ execute/execute.c
@@ -1,5 +1,12 @@
 #include "parser.h"
 
+typedef struct s_env
+{
+	char	*key;
+	char	*value;
+	t_env	*next;
+}	t_env;
+
 typedef struct s_command
 {
 	char				*command;
@@ -10,6 +17,19 @@ typedef struct s_command
 	char 				*redirect_append;
 	struct s_command	*next;
 }				t_command;
+
+typedef struct s_parser
+{
+	int					exit_status;
+	int					nb_pipes;
+	int					infile_fd;
+	int					outfile_fd;
+	char				*infile;
+	char				*outfile;
+	struct s_command	commands;
+	struct s_env		envs;
+	struct s_parser		*next;
+}				t_parser;
 
 typedef enum e_builtin
 {
@@ -33,15 +53,6 @@ example: echo  "kimia" | ls |  grep  "b";
 command => tokenlist
 */
 
-/* it will execute the command with its path
-	-finding paths
-	-*/
-
-void	run_path_cmd(t_command *command)
-{
-
-}
-
 /* compare the string to builtins 
 	return value: enum of type builin*/
 
@@ -64,49 +75,51 @@ static t_builtin	get_builtin_command(const char *cmd)
 	return (UNKNOWN);
 }
 
-/* check the first token of the commad
-	if it is a builtin call its function to execute
-	if not call the function to execute with its path
-	return value: undefined(maybe it should be the exit status with
-	return value of the builtins function or exit status that stored
-	with waitpid in path commands)*/
-
-void	check_builtin(t_command *command)
+bool	check_builtin(t_command *command)
 {
 	char	*cmd;
 
 	// echo  "kimia" --- cmd = echo;
 	cmd = command->tokens[0]->token->value;
 	if (cmd == ECH)
-		return (ft_echo(command));
+		return (ft_echo(command), true);
 	if (cmd == CD)
-		return (ft_cd(command));
+		return (ft_cd(command), true);
 	if (cmd == PWD)
-		return (ft_pwd(command));
+		return (ft_pwd(command), true);
 	if (cmd == ENV)
-		return (ft_env(command));
+		return (ft_env(command), true);
 	if (cmd == EXPORT)
-		return (ft_export(command));
+		return (ft_export(command), true);
 	if (cmd == UNSET)
-		return (ft_unset(command));
+		return (ft_unset(command), true);
 	if (cmd == EXIT)
-		return (ft_exit(command));
-	return (run_path_cmd(command));
+		return (ft_exit(command), true);
+	return (false);
 }
 
-/* start of the execution 
-	loop through the commands and call the function to 
-	check if it is builtin or not and execute them
-	return value: undefined */
+/*
+	-start of execution
+	-check the number of pipes for execution
+	-call their functions and store and retuen the exit status
+*/
 
-void	ft_execute(t_command *commands)
+int	ft_execute(t_parser *parser)
 {
-	t_command	*temp;
+	int	number_pipe;
 
-	temp = commands;
-	while (temp->command)
+	number_pipe = parser->nb_pipes;
+	if (number_pipe == 0)
 	{
-		check_builtin(temp->command);
-		temp->command = temp->command->next;
+		if (check_builtin(parser) == false)
+		{
+			// find/have/check path
+			parser->exit_status = execute_one_cmd(parser);
+		}
 	}
+	else if (number_pipe >= 1)
+	{
+		parser->exit_status = pipline(parser);
+	}
+	return (parser->exit_status);
 }
