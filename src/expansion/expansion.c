47dc99ac1d677c99130c5f5147de0bc9be5795c9
Y4ZK4Y <yk@yasamankarimi@gmail.com>
2024-12-19 14:40:44 +0100

    fix norminette for expansion

--- src/expansion/expansion.c
+++ src/expansion/expansion.c
@@ -6,7 +6,7 @@
 /*   By: ykarimi <ykarimi@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/09/12 17:17:33 by ykarimi       #+#    #+#                 */
-/*   Updated: 2024/12/19 14:33:26 by ykarimi       ########   odam.nl         */
+/*   Updated: 2024/12/19 14:38:47 by ykarimi       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ char	*replace_variable(char *expanded, char *var_start, char *var_value)
 	size_t	var_name_len;
 
 	var_name_len = 0;
-	while (ft_isalnum(var_start[var_name_len + 1]) || var_start[var_name_len + 1] == '_')
+	while (ft_isalnum(var_start[var_name_len + 1]) || \
+	var_start[var_name_len + 1] == '_')
 		var_name_len++;
 	before_var = ft_substr(expanded, 0, var_start - expanded);
 	after_var = ft_strdup(var_start + var_name_len + 1);
@@ -38,51 +39,51 @@ char	*replace_variable(char *expanded, char *var_start, char *var_value)
 
 static char	*skip_single_quotes(char *expanded, char *var_start)
 {
-    char	*single_quote_start;
-    char	*single_quote_end;
+	char	*single_quote_start;
+	char	*single_quote_end;
 
-    single_quote_start = ft_strchr(expanded, '\'');
-    if (single_quote_start && single_quote_start < var_start)
-    {
-        single_quote_end = ft_strchr(single_quote_start + 1, '\'');
-        if (single_quote_end && single_quote_end > var_start)
-        {
-            var_start = ft_strchr(single_quote_end + 1, '$');
-        }
-    }
-    return (var_start);
+	single_quote_start = ft_strchr(expanded, '\'');
+	if (single_quote_start && single_quote_start < var_start)
+	{
+		single_quote_end = ft_strchr(single_quote_start + 1, '\'');
+		if (single_quote_end && single_quote_end > var_start)
+		{
+			var_start = ft_strchr(single_quote_end + 1, '$');
+		}
+	}
+	return (var_start);
 }
 
 static char	*process_variable(char *expanded, char *var_start, char **envp)
 {
-    char	*var_name;
-    char	*var_value;
+	char	*var_name;
+	char	*var_value;
 
-    var_name = get_var_name(var_start);
-    var_value = get_env_value(var_name, envp);
-    free(var_name);
-    if (var_value)
-    {
-        expanded = replace_variable(expanded, var_start, var_value);
-        free(var_value);
-    }
-    return (expanded);
+	var_name = get_var_name(var_start);
+	var_value = get_env_value(var_name, envp);
+	free(var_name);
+	if (var_value)
+	{
+		expanded = replace_variable(expanded, var_start, var_value);
+		free(var_value);
+	}
+	return (expanded);
 }
 
 char	*expand_variables(const char *input, char **envp)
 {
-    char	*expanded;
-    char	*var_start;
+	char	*expanded;
+	char	*var_start;
 
-    expanded = ft_strdup(input);
-    var_start = ft_strchr(expanded, '$');
-    while (var_start)
-    {
-        var_start = skip_single_quotes(expanded, var_start);
-        if (!var_start)
-            break;
-        expanded = process_variable(expanded, var_start, envp);
-        var_start = ft_strchr(expanded, '$');
-    }
-    return (expanded);
+	expanded = ft_strdup(input);
+	var_start = ft_strchr(expanded, '$');
+	while (var_start)
+	{
+		var_start = skip_single_quotes(expanded, var_start);
+		if (!var_start)
+			break ;
+		expanded = process_variable(expanded, var_start, envp);
+		var_start = ft_strchr(expanded, '$');
+	}
+	return (expanded);
 }
--- src/expansion/expansion_utils.c
+++ src/expansion/expansion_utils.c
@@ -6,7 +6,7 @@
 /*   By: ykarimi <ykarimi@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/12/19 14:31:38 by ykarimi       #+#    #+#                 */
-/*   Updated: 2024/12/19 14:34:03 by ykarimi       ########   odam.nl         */
+/*   Updated: 2024/12/19 14:39:46 by ykarimi       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,16 @@
 
 char	*get_env_value(const char *var_name, char **envp)
 {
-	size_t len;
+	size_t	len;
 	int		i;
 
 	i = 0;
 	len = ft_strlen(var_name);
-	while(envp[i])
+	while (envp[i])
 	{
 		if (ft_strncmp(envp[i], var_name, len) == 0 && envp[i][len] == '=')
 			return (ft_strdup(envp[i] + len + 1));
 		i++;
-		
 	}
 	return (ft_strdup(""));
 }
@@ -40,4 +39,3 @@ char	*get_var_name(const char *var_start)
 		var_end++;
 	return (ft_substr(var_start + 1, 0, var_end - var_start - 1));
 }
-
